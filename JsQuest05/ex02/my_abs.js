4b68802065c9abf3684f2ce3c28623a2ee40fb05
Saint-Dave <babaloladavid7@gmail.com>
2022-10-13 21:20:19 +0100

    nice work

--- JsQuest05/ex02/my_abs.js
+++ JsQuest05/ex02/my_abs.js
@@ -1,9 +1,20 @@
 // Convert negative numbers to positive numbers
 
-function my_abs(integer){
-    if (integer<0) {
-        Integer = integer * -1;
+// function my_abs(integer){
+//     if (integer<0) {
+//         Integer = integer * -1;
+//     }
+//     return Integer + 12
+// }
+function my_abs(param_1){
+    if (param_1<0) {
+        return param_1 * -1; 
     }
-    return Integer + 12
+    if (param_1>=0) {
+        return param_1;
+    }
 }
 console.log(my_abs(-30));
+console.log(my_abs(30));
+console.log(my_abs(-9));
+console.log(my_abs(99));
