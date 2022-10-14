348fa5a74bae3752033f4e1df69307c137f677fc
Saint-Dave <babaloladavid7@gmail.com>
2022-10-14 12:58:23 +0100

    done with the code

--- JsQuest05/ex06/my_string_formatting.js
+++ JsQuest05/ex06/my_string_formatting.js
@@ -1,24 +1,3 @@
-// function my_string_formatting(firstname, lastname, age) {
-//    firstname = "John";
-//    lastname = "Doe";
-//    age = "37.";
-//  };
-
-//  function my_string_formatting(firstname) {
-//     return "John";
-//   };
-
-// console.log("Hello, my name is" + my_string_formatting(firstname) + my_string_formatting(lastname)+", I'm" +my_string_formatting(age)+ ".");              
-
 function my_string_formatting(firstname, lastname,age) {
-    var firstname = 'John';
-    var lastname = 'Doe';
-    var age = 37;
-
-    var string =
-    "Hello, my name is " + String(firstname) + " " +
-        String(lastname)+", I'm " + String(age) + ".";
-
-    console.log(string);
+    console.log("Hello, my name is " + firstname + " " + lastname + ",I'm " + age + ".");
 }
-my_string_formatting();
